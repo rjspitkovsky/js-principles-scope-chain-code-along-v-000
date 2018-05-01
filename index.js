b5d3e3dee4d4fbb78d8771cbcca2f9d6e2e48116
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 23:00:21 +0000

    Done.

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ function overWriteBestCustomer(newFavorite){
   bestCustomer = newFavorite
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
     let favoriteCustomer = 'not bar'
